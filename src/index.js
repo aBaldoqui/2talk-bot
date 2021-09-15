cb4d913cad8be2e50d2294a17e9bd6c986cb8ca0
Arthur Baldoqui <arthur.b.bergamo@gmail.com>
2021-09-15 18:46:02 -0300

    feiao oq eu to fazendo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+console.log('sim')
+
 const {messageHandler, reactionHandler, innit} = require('./handlers/index.js');
 const {Client, Intents} = require('discord.js');
 
@@ -5,6 +7,8 @@ require('dotenv').config()
 
 const token = "ODc4NzEyNzE1ODM5OTQ2ODIy.YSFK_Q.FmFQQLxVfPubA1-iqsVqdH4K7Pk";
 const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES", "GUILD_MESSAGE_REACTIONS"] });
+
+console.log('vamo bora')
 
 try{
     client.once('ready', innit);
