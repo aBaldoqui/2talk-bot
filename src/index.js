df2630a676f17dc88935ec354bd4e8144ceeee81
Arthur Baldoqui <arthur.b.bergamo@gmail.com>
2021-09-15 18:41:12 -0300

    add try catch in index.js

--- src/index.js
+++ src/index.js
@@ -6,11 +6,14 @@ require('dotenv').config()
 const token = "ODc4NzEyNzE1ODM5OTQ2ODIy.YSFK_Q.FmFQQLxVfPubA1-iqsVqdH4K7Pk";
 const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES", "GUILD_MESSAGE_REACTIONS"] });
 
+try{
+    client.once('ready', innit);
 
-client.once('ready', innit);
-
-client.on('messageReactionAdd', reactionHandler)
-
-client.on('messageCreate', messageHandler)
-
-client.login(token);
+    client.on('messageReactionAdd', reactionHandler)
+    
+    client.on('messageCreate', messageHandler)
+    
+    client.login(token);
+}catch(err){
+    console.log(err)
+}
