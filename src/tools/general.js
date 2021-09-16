0c236591d7a78626bc649680f2d43093be9f1f3a
Arthur Baldoqui <arthur.b.bergamo@gmail.com>
2021-09-16 19:06:02 -0300

    fix exit function

--- src/tools/general.js
+++ src/tools/general.js
@@ -3,19 +3,28 @@ const { Message } = require('discord.js');
 const {exitMsg} = require('./embedMsgs')
 
 const exit = async (msg)=>{
-    await msg.channel.send({embeds: [exitMsg]})
+    await msg.channel.send({ embeds: [exitMsg] })
 
     await msg.channel.permissionOverwrites.set([
-    {
-        id : msg.author.id,
-        deny: ['SEND_MESSAGES'],
-    }])
+        {
+            id: msg.channel.guild.roles.everyone,
+            deny: ['SEND_MESSAGES', 'VIEW_CHANNEL', 'READ_MESSAGE_HISTORY'],
+        },
+        {
+            id: msg.author.id,
+            deny: ['SEND_MESSAGES'],
+        }
+    ])
 
-    setTimeout(function(){ 
-        msg.channel.permissionOverwrites.set([
+    setTimeout(async function () {
+        await msg.channel.permissionOverwrites.set([
             {
-                id : msg.author.id,
-                deny: ['VIEW_CHANNEL'],
+                id: msg.channel.guild.roles.everyone,
+                deny: ['SEND_MESSAGES', 'VIEW_CHANNEL'],
+            },
+            {
+                id: msg.author.id,
+                deny: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'],
             }])
     }, 3000);
 }
