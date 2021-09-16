d9cd04c4b19595b15360c3e1e4b93f76d34c3882
Arthur Baldoqui <arthur.b.bergamo@gmail.com>
2021-09-15 23:16:25 -0300

    exit function

---
+++ src/tools/general.js
@@ -0,0 +1,25 @@
+const { Message } = require('discord.js');
+
+const {exitMsg} = require('./embedMsgs')
+
+const exit = async (msg)=>{
+    await msg.channel.send({embeds: [exitMsg]})
+
+    msg.channel.permissionOverwrites.set([
+    {
+        id : msg.author.id,
+        deny: ['SEND_MESSAGES'],
+    }])
+
+    setTimeout(function(){ 
+        msg.channel.permissionOverwrites.set([
+            {
+                id : msg.author.id,
+                deny: ['VIEW_CHANNEL'],
+            }])
+    }, 3000);
+}
+
+module.exports = {
+    exit,
+}
